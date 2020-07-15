e4c2271c0a1194e7b061d5cb63f95e45001df6ec
Tatyana Vogel <tvogel@adobe.com>
2020-07-15 09:32:43 +0200

    Shared Seeds works

--- src/components/__tests__/SelectContainer.test.js
+++ src/components/__tests__/SelectContainer.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, waitForElement, getAllByTestId } from '@testing-library/react'
+import { render, waitForElement } from '@testing-library/react'
 import SelectContainer from '../SelectContainer'
 import { TimetableData } from '../TimetableContext'
 import { startServer } from '../server'
@@ -18,7 +18,13 @@ afterEach(() => {
 })
 
 test('render the select container and manage the view', async () => {
-  const { queryByLabelText, getByLabelText, debug, getByTestId } = render(
+  const {
+    queryByLabelText,
+    getByLabelText,
+    debug,
+    getByTestId,
+    getAllByTestId,
+  } = render(
     <TimetableData>
       <SelectContainer />
     </TimetableData>
@@ -27,15 +33,13 @@ test('render the select container and manage the view', async () => {
   const jobSelect = getByLabelText(/job/i)
   const courseSelect = queryByLabelText(/course/i)
 
-  console.log(server.db.jobs)
-  // const options = await waitForElement(() => getByLabelText(/job/i))
   const options = await waitForElement(() => getAllByTestId('selectOption'))
   debug(options)
 
   expect(header).toHaveTextContent(/stundenplan gibm/i)
   expect(jobSelect).toHaveTextContent(/please select your job/i)
   expect(courseSelect).toBeNull()
-  expect(options).toHaveLenght(3)
+  expect(options).toHaveLength(3)
 
   //userEvent.selectOptions(jobSelect, )
 })
